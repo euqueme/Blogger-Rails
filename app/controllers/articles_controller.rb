eb9b8996702aa5300c017e490f6a4fa60f682afd
euqueme <euqueme@gmail.com>
2019-11-28 14:39:58 -0600

    FORM-BASED WORKFLOW
    feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,34 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
     @articles = Article.all
   end
   def show
     @article = Article.find(params[:id])
   end
+  def new
+    @article = Article.new
+  end
+  def create
+    @article = Article.new(article_params)
+    @article.save
+	flash.notice = "Article '#{@article.title}' Created!"
+    redirect_to article_path(@article)
+  end
+  def destroy    
+    @article = Article.find(params[:id])	
+	title = @article.title
+	@article.destroy
+	flash.notice = "Article '#{title}' Destroyed!"
+	redirect_to articles_path
+  end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+	flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
+end
 end
