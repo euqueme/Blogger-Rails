06f9d0a78556233bd4345f5d843031c3421f1b03
euqueme <euqueme@gmail.com>
2019-11-29 15:42:48 -0600

    TAGGING FEATURE
    Completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
+  has_many :taggings
+  has_many :tags, through: :taggings, dependent: :destroy
+  def tag_list
+    self.tags.collect do |tag|
+      tag.to_s
+    end
+  end
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+	new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
 end
